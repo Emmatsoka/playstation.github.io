210cd8406f3c536ec5101826846797150edbc840
Emmatsoka <emmatsoka.qwq@gmail.com>
2023-05-27 16:40:49 +0200

    .

--- scripts/script.js
+++ scripts/script.js
@@ -28,8 +28,42 @@ function scrollFade() {
 	}
 }
 
+
 document.addEventListener('scroll', scrollFade);
 window.addEventListener('resize', scrollFade);
 document.addEventListener('DOMContentLoaded', function() {
     scrollFade();
 });
+
+var temavaltoikon = document.getElementById("temavalto");
+var pslogo = document.getElementById('pslogo');
+var tema;
+
+let temamod = localStorage.getItem("temamod");
+if (temamod === 'vilagos') {
+	document.body.classList.add("vilagos-tema");
+	if (temamod === 'vilagos') {
+		temavaltoikon.src = "./images/hold.png";
+		pslogo.src = "./images/playblack.png";
+	} else {
+		document.body.classList.add("");
+		temavaltoikon.src = "./images/nap.png";
+		pslogo.src = "./images/playwhite.png";
+	}
+}
+
+function temavaltas() {
+	document.body.classList.toggle("vilagos-tema");
+	if (document.body.classList.contains("vilagos-tema")) {
+		temavaltoikon.src = "./images/hold.png";
+		pslogo.src = "./images/playblack.png";
+		tema = 'vilagos';
+	} else {
+		temavaltoikon.src = "./images/nap.png";
+		pslogo.src = "./images/playwhite.png";
+		tema = 'sotet';
+	}
+
+	// Mentjük a téma értékét a localStorage-ba
+	localStorage.setItem("temamod", tema);
+}
